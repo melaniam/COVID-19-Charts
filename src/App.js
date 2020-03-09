d2ad295b077357ccd4b5c23887c551987427ce70
melaniam <melaniam@users.noreply.github.com>
2020-03-09 23:08:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+
+import Countries from "./Countries";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>COVID-2019 Data Charts</h1>
+      <h2>First 60 countries sorted by confirmed cases, in descending order</h2>
+      <p>
+        <a
+          href="https://codesandbox.io/s/covid-19-data-chart-c3ewv"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Link to Code"
+        >
+          Check out the code
+        </a>
+        {" and the "}
+        <a
+          href="https://github.com/CSSEGISandData/COVID-19/issues/295"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Link to data source"
+        >
+          data source
+        </a>
+      </p>
+
+      <Countries />
+    </div>
+  );
+}
